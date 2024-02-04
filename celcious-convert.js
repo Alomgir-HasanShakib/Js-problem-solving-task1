a82fc7805db3b8c38c28306f0a2f2b85058aa330
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2024-02-04 20:00:51 +0600

    solved calsius converter problems task

--- celcious-convert.js
+++ celcious-convert.js
@@ -1,3 +1,9 @@
 // Task-1:
 // Write a function to convert temperature from Celsius to Fahrenheit.
 
+function celsiusToFahrenheit(calsius) {
+  const fahrenheit = calsius * 1.8 + 32;
+  return fahrenheit;
+}
+const checkFahrenheit = celsiusToFahrenheit(3);
+console.log(checkFahrenheit);
