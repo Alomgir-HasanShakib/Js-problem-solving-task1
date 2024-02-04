07fdb143757c6c4d3c12ea33e510b976937ba643
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2024-02-04 22:02:55 +0600

    solved repeated numbers problems task

--- repeat-num-count.js
+++ repeat-num-count.js
@@ -7,7 +7,6 @@
 
 // output: 2
 
-
 // sample-input:
 
 // numbers = [5,6,11,12,98, 5]
@@ -15,3 +14,19 @@
 // find: 25
 
 // output: 0
+
+function repeatedNumbers(arr, target) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    if (element === target) {
+      count++;
+    } else {
+      count = 0;
+    }
+  }
+  return count;
+}
+
+const arr = [5, 7, 8, 20, 20, 50, 5, 4, 20, 70, 210, 20, 20];
+console.log(repeatedNumbers(arr, 50));
