17e758003ddbf0bf092d78d3ca78c75af5f3a15a
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2024-02-04 20:09:33 +0600

    solved vowel counter problems task

--- vowel-count.js
+++ vowel-count.js
@@ -1,2 +1,18 @@
 // Task-3:
 // Write a function to count the number of vowels in a string.
+
+function vowelCounter(string) {
+  let count = 0;
+  const vowels = ["a", "e", "i", "o", "u"];
+  for (const word of string) {
+    // console.log(word);
+    for (const vowel of vowels) {
+      if (word === vowel) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+const numberOfVowels = vowelCounter("aeiou");
+console.log(numberOfVowels);
