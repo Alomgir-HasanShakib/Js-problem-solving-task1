0e38521c766e1c15ff479cfd887ab06e0e0c18cc
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2024-02-04 20:40:04 +0600

    solved longest word finder problems task

--- largest-word.js
+++ largest-word.js
@@ -4,3 +4,18 @@
 // sample-input: I am learning Programming to become a programmer
 
 // sample-output: Programming
+
+function longestWord(string) {
+  const spliteStr = string.split(" ");
+  let contentlongestWord = " ";
+  for (const word of spliteStr) {
+    if (word.length > contentlongestWord.length) {
+      contentlongestWord = word;
+    }
+  }
+  return contentlongestWord;
+}
+const stringOfWords = longestWord(
+  "I am learning Programming to become a programmer"
+);
+console.log(stringOfWords);
